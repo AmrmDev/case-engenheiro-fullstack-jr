e1568aef77975f12591a216c777cb3665f785c5c
Pedro Amorim <amrm.developer@gmail.com>
2026-03-18 17:51:38 -0300

    fixing database engine

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,14 +2,12 @@ import os
 import pytest
 from fastapi.testclient import TestClient
 
-# Seta ANTES de qualquer import da app
 os.environ["TEST_DATABASE_URL"] = "sqlite:///./test.db"
 
 import app.database as db_module
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Substitui o engine diretamente no módulo
 test_engine = create_engine("sqlite:///./test.db", connect_args={"check_same_thread": False})
 db_module.engine = test_engine
 db_module.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
